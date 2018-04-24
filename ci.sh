0a2f6bf24b773958f96ff03b95ef6bd6560a3c18
hideBerry <hide@berry>
2018-04-24 22:34:33 +0000

    add ci

---
+++ ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function start {
+  screen -dmS sphere
+  screen -S sphere -X stuff "/home/pi/KDS-Core/spheresvr-arm^M"
+}
+
+function stop {
+  save
+  screen -S sphere -X quit
+}
+
+function save {
+  screen -S sphere -X stuff "save^M"
+}
+
+function resync {
+  screen -S sphere -X stuff "resync^M"
+}
+
+$1
